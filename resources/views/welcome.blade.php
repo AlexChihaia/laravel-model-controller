acbd193fcdf261dbc257b339939183ecb50f9c3e
AlexChihaia <alexchihaia84@gmail.com>
2024-01-29 17:41:06 +0100

    feat: simple design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Template</title>
+    <title>Laravel Model Controller</title>
 
     @vite('resources/js/app.js')
 </head>
+
+
+
+    <div class="container bg-secondary">
+        <div class="row">
+            @foreach($movies as $movie)
+                <div class="col-md-3 py-5">
+                    <div class="card bg-primary">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text">Released in: {{ $movie->date }}</p>
+                            <p class="card-text">Production nationality: {{ $movie->nationality }}</p>
+                            <p class="card-text">Vote: {{ $movie->vote }}</p>
+                            <p class="card-text">Original title: {{ $movie->original_title }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
 
 <body>
 
